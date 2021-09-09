8dc3f7241104994aecc4cb2fba0cc2a29601fae6
daimond113 <72147841+daimond113@users.noreply.github.com>
2021-09-09 13:24:36 +0200

    Try to fix actions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import Header from '../components/Header'
-import Link from '../components/Link'
-import Section from '../components/Section'
 import Head from 'next/head'
 
 export default function Home() {
